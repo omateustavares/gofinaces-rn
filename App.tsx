2584a4af01f86b58bfcb876b423ec3d8757fffb7
Mateus Tavares <mateustm17@gmail.com>
2022-09-14 23:15:46 -0300

    chore: initial config

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
+import * as SplashScreen from "expo-splash-screen";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+import { Register } from "./src/screens/Register";
+import theme from "./src/global/styles/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Register />
+    </ThemeProvider>
+  );
+}
