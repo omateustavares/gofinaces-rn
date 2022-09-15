fe270ececf1f1464ec685c140dec7c7beac5571b
Mateus Tavares <mateustm17@gmail.com>
2022-09-14 23:12:28 -0300

    chore: components

---
+++ src/components/TransactionCard/index.tsx
@@ -0,0 +1,48 @@
+import {
+  Container,
+  Title,
+  Amount,
+  Footer,
+  Category,
+  Icon,
+  CategorieName,
+  Date,
+} from "./styles";
+
+interface Category {
+  name: string;
+  icon: string;
+}
+
+export interface TransactionCardProps {
+  type: "positive" | "negative";
+  title: string;
+  amount: string;
+  category: Category;
+  date: string;
+}
+
+interface Props {
+  data: TransactionCardProps;
+}
+
+export function TransactionCard({ data }: Props) {
+  return (
+    <Container>
+      <Title>{data.title}</Title>
+      <Amount type={data.type}>
+        {data.type === "negative" && "- "}
+        {data.amount}
+      </Amount>
+
+      <Footer>
+        <Category>
+          <Icon name={data.category.icon} />
+
+          <CategorieName>{data.category?.name}</CategorieName>
+        </Category>
+        <Date>{data.date}</Date>
+      </Footer>
+    </Container>
+  );
+}
