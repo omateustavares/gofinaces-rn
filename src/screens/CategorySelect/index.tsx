e4a11ae6fc9fddbdb811b35b0a28d2b5516f72c6
Mateus Tavares <mateustm17@gmail.com>
2022-09-14 23:15:32 -0300

    chore: page category select

---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,62 @@
+import { FlatList } from "react-native";
+import { categories } from "../../../categories";
+import { Button } from "../../components/Form/Button";
+import {
+  Category,
+  Container,
+  Footer,
+  Header,
+  Icon,
+  Name,
+  Separator,
+  Title,
+} from "./styles";
+
+interface Category {
+  key: string;
+  name: string;
+}
+
+interface Props {
+  category: Category;
+  setCategory: (category: Category) => void;
+  closeSelectcategory: () => void;
+}
+
+export function CategorySelect({
+  category,
+  setCategory,
+  closeSelectcategory,
+}: Props) {
+  function handleCategorySelect(category: Category) {
+    setCategory(category);
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Title>Categoria</Title>
+      </Header>
+
+      <FlatList
+        data={categories}
+        style={{ flex: 1, width: "100%" }}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <Category
+            onPress={() => handleCategorySelect(item)}
+            isActive={category.key === item.key}
+          >
+            <Icon name={item.icon} />
+            <Name>{item.name}</Name>
+          </Category>
+        )}
+        ItemSeparatorComponent={() => <Separator />}
+      />
+
+      <Footer>
+        <Button title="Selecitonar" onPress={closeSelectcategory} />
+      </Footer>
+    </Container>
+  );
+}
